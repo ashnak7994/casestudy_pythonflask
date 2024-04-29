d2ccfbfcbd42eef93607b2ff8b03b87207c60a44
ashnak7994 <157314040+ashnak7994@users.noreply.github.com>
2024-04-29 09:51:37 +0000

    1

---
+++ app.py
@@ -0,0 +1,22 @@
+from flask import Flask,render_template, request
+
+import pickle
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/predict', methods=['POST'])
+def predict():
+    if request.method == 'POST':
+        model = pickle.load(open('model.pkl', 'rb'))
+        age = int(request.form['age'])
+        salary = int(request.form['salary'])
+        prediction = model.predict([[age, salary]])
+        result = "Will Purchase" if prediction[0] == 1 else "Will Not Purchase"
+        return render_template('result.html', result=result)
+
+if __name__ == '__main__':
+    app.run(debug=True)
